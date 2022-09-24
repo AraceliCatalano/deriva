d0dccd2840fcac40bdcdec91a6ce2f318e83b3df
“AraceliCatalano” <aracelicatalano@gmail.com>
2022-09-24 10:21:18 -0300

    Desafio 8 (cart contect) + desafío 9 (cart view) y Login de Firebase

--- src/components/loginComponents/HomeLogin.js
+++ src/components/loginComponents/HomeLogin.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Container } from 'react-bootstrap';
+import { Button, Container, Row, Col, Table } from 'react-bootstrap';
 import { useNavigate } from "react-router";
 import { useUserAuth } from '../../context/UserAuthContext';
 
@@ -17,13 +17,134 @@ const HomeLogin = () => {
     }
     return (
         <>
-            <Container className="p-4 box"> 
-            Perfil de usuario<br />
-            {user && user.email}
-            <hr/>
-            <Button variant="dark" onClick={handleLogout}>Cerrar sesión</Button>
+            <Container className="p-4 "> 
+            <Row>
+                <Col sm={2}>
+                    <img className="avatar" src="https://firebasestorage.googleapis.com/v0/b/deriva-debe3.appspot.com/o/avatars%2FDefaultAvatar.png?alt=media&token=d480b693-fad2-415a-8674-ed87155d0ee2" alt="default avatar" />
+                </Col>
+                <Col sm={8}>
+                    <Row >
+                    <h3>Nombre del usuario</h3>
+                     <h5 style={{marginLeft: 0}}>{user && user.email}</h5>    
+                    </Row>
+                </Col>
+                <Col sm={2}>
+                    <Button variant="dark" onClick={handleLogout}>Cerrar sesión</Button>
+                </Col>
+            </Row>
             </Container>
-            
+            <hr/>
+            <Container className="cart-container p-3">
+                <Row>
+                    <Col sm={8}>
+                        <h3>Tu lista de paseos</h3>
+                    </Col>
+                    <Col sm={4}>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col sm={8}>
+                        <Table responsive="sm">
+                            <thead>
+                            <tr>
+                                <th>Paseo guardado</th>
+                                <th>Desplegar</th>
+                                <th>Agregar nota</th>
+                                <th>Compartir</th>
+                                <th>Eliminar </th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                   
+                            <tr>
+                                <td> Nombre del paseo</td>
+                                <td> 
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-box-arrow-in-right" viewBox="0 0 16 16">
+                                        <path fill-rule="evenodd" d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0v-2z"/>
+                                         <path fill-rule="evenodd" d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
+                                    </svg>
+                                </td>
+                                <td>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-card-list" viewBox="0 0 16 16">
+                                    <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"/>
+                                    <path d="M5 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 5 8zm0-2.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm0 5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-1-5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zM4 8a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zm0 2.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0z"/>
+                                    </svg>
+                                </td>
+                                <td>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-share-fill" viewBox="0 0 16 16">
+                                        <path d="M11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.499 2.499 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5z"/>
+                                    </svg>
+                                </td>
+                                <td>
+                                    <Button variant="danger" > 
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-trash-fill" viewBox="0 0 16 16">
+                                            <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+                                        </svg>
+                                    </Button>
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td> Nombre del paseo</td>
+                                <td> 
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-box-arrow-in-right" viewBox="0 0 16 16">
+                                        <path fill-rule="evenodd" d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0v-2z"/>
+                                         <path fill-rule="evenodd" d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
+                                    </svg>
+                                </td>
+                                <td>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-card-list" viewBox="0 0 16 16">
+                                    <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"/>
+                                    <path d="M5 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 5 8zm0-2.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm0 5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-1-5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zM4 8a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zm0 2.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0z"/>
+                                    </svg>
+                                </td>
+                                <td>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-share-fill" viewBox="0 0 16 16">
+                                        <path d="M11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.499 2.499 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5z"/>
+                                    </svg>
+                                </td>
+                                <td>
+                                    <Button variant="danger" > 
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-trash-fill" viewBox="0 0 16 16">
+                                            <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+                                        </svg>
+                                    </Button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td> Nombre del paseo</td>
+                                <td> 
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-box-arrow-in-right" viewBox="0 0 16 16">
+                                        <path fill-rule="evenodd" d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0v-2z"/>
+                                         <path fill-rule="evenodd" d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
+                                    </svg>
+                                </td>
+                                <td>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-card-list" viewBox="0 0 16 16">
+                                    <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"/>
+                                    <path d="M5 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 5 8zm0-2.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm0 5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-1-5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zM4 8a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zm0 2.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0z"/>
+                                    </svg>
+                                </td>
+                                <td>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-share-fill" viewBox="0 0 16 16">
+                                        <path d="M11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.499 2.499 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5z"/>
+                                    </svg>
+                                </td>
+                                <td>
+                                    <Button variant="danger" > 
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-trash-fill" viewBox="0 0 16 16">
+                                            <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+                                        </svg>
+                                    </Button>
+                                </td>
+                            </tr>
+
+
+                            </tbody>
+                        </Table>
+                    </Col>
+                    </Row>
+            </Container>
            
         </>
     );
--- src/routes/Generic.js
+++ src/routes/Generic.js
@@ -3,29 +3,48 @@ import { Routes, Route } from 'react-router-dom';
 import Home from '../components/home/Home'
 import ItemDetailContainer from '../components/catalog/ItemDetailContainer';
 import Cart from '../components/catalog/Cart';
-import BonusTrackMain from '../components/bonusTrack/BonusTrackMain';
-import Help from '../components/footer/Help'
-import PrivacyPolicy from '../components/footer/PrivacyPolicy'
-import About from '../components/footer/About'
-import TermsAndConditions from '../components/footer/TermsAndConditions'
-//import ItemListContainer from '../components/catalog/ItemListContainer';
+import BonusTrackMain from '../components/BonusTrack/BonusTrackMain';
+import Help from '../components/footer/Help';
+import PrivacyPolicy from '../components/footer/PrivacyPolicy';
+import About from '../components/footer/About';
+import TermsAndConditions from '../components/footer/TermsAndConditions';
 import Category from '../components/home/Category'
+//import ItemListContainer from '../components/catalog/ItemListContainer';
+
+// Imports for Login with Firebase
+import { UserAuthContextProvider } from '../context/UserAuthContext';
+import Login from '../components/loginComponents/Login';
+import ForgotPassword from '../components/loginComponents/ForgotPassword';
+import SignUp from '../components/loginComponents/SignUp';
+import HomeLogin from '../components/loginComponents/HomeLogin'
+import ProtectedRoute from './ProtectedRoute';
 
 function Generic() {
   return (
-   <>       
-    <Routes>
-        <Route path="/" element={<Home />} />    
-        {/* <Route exact path="/paseos" element={<ItemListContainer />} />   */}
-        <Route exact path="/categoria/:categoryId/:categoryName" element={<Category />} /> 
-        <Route exact path="/item/:id" element={<ItemDetailContainer />} />
-        <Route exact path="/cart" element={<Cart />} />
-        <Route exact path="/bonusTracks" element={<BonusTrackMain />} />  
-        <Route exact path="/ayuda" element={<Help />} /> 
-        <Route exact path="/sobre-deriva" element={<About />} /> 
-        <Route exact path="/terminos-y-condiciones" element={<TermsAndConditions />} /> 
-        <Route exact path="/politica-privacidad" element={<PrivacyPolicy />} />                
-    </Routes>        
+   <>   
+      <UserAuthContextProvider>
+      <Routes>
+          <Route path="/" element={<Home />} />    
+          {/* <Route exact path="/paseos" element={<ItemListContainer />} />   */}
+          <Route exact path="/categoria/:categoryId/:categoryName" element={<Category />} /> 
+          <Route exact path="/item/:id" element={<ItemDetailContainer />} />
+          <Route exact path="/cart" element={<Cart />} />
+          <Route exact path="/bonusTracks" element={<BonusTrackMain />} />  
+          <Route exact path="/ayuda" element={<Help />} /> 
+          <Route exact path="/sobre-deriva" element={<About />} /> 
+          <Route exact path="/terminos-y-condiciones" element={<TermsAndConditions />} /> 
+          <Route exact path="/politica-privacidad" element={<PrivacyPolicy />} />  
+          <Route 
+                    exact path="/homeLogin" 
+                    element={
+                      <ProtectedRoute> 
+                        <HomeLogin />
+                      </ProtectedRoute>} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/signup" element={<SignUp />} />
+          <Route exact path="/restablecerContrasena" element={<ForgotPassword />} />                  
+      </Routes>        
+    </UserAuthContextProvider>    
    </>   
   );
 }
